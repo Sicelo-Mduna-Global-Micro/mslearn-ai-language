741dedec9bac328c187b96952389f37c1503a6ea
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-30 14:34:44 +0200

    Add code to extract entities

--- Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net8.0/text-analysis.AssemblyInfo.cs
+++ Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net8.0/text-analysis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e705d98754308545847375437dd9769a401aa2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9d495f3bc63c592b7d4b00b762de47b5fdd879")]
 [assembly: System.Reflection.AssemblyProductAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
