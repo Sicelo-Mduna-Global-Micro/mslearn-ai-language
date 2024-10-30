ce87d275200d2781de794c0af42c8718a5bbab81
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-30 14:14:15 +0200

    install the Azure AI Language Text Analytics SDK package

--- Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
+++ Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8996005b36ec8f88cf841fe5c3362917aaa782bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e1b5ba1a132e92d568fd2c95bc3f0f92f37aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
