8e79ed55482a056cf7ab0f28e2f9decfa7d63eff
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-11-13 19:57:54 +0200

    commit

--- Labfiles/04-text-classification/C-Sharp/classify-text/obj/Debug/net7.0/classify-text.AssemblyInfo.cs
+++ Labfiles/04-text-classification/C-Sharp/classify-text/obj/Debug/net7.0/classify-text.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("classify-text")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec3b9e244e722350272421e819c1217097028ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf6cfbd648076810d02dfe69410a5c5f7f0d85b")]
 [assembly: System.Reflection.AssemblyProductAttribute("classify-text")]
 [assembly: System.Reflection.AssemblyTitleAttribute("classify-text")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Labfiles/05-custom-entity-recognition/C-Sharp/custom-entities/obj/Debug/net7.0/custom-entities.AssemblyInfo.cs
+++ Labfiles/05-custom-entity-recognition/C-Sharp/custom-entities/obj/Debug/net7.0/custom-entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("custom-entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec3b9e244e722350272421e819c1217097028ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf6cfbd648076810d02dfe69410a5c5f7f0d85b")]
 [assembly: System.Reflection.AssemblyProductAttribute("custom-entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("custom-entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
